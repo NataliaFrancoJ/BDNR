fe584363f4654111a2ab2adf172461b2fb3db911
Julieta <julilopezlicio06@gmail.com>
2025-12-04 17:41:32 -0300

    carga de datos validos

--- ObligatorioBDNR/ObligatorioBDNR/obj/Debug/net8.0/ObligatorioBDNR.AssemblyInfo.cs
+++ ObligatorioBDNR/ObligatorioBDNR/obj/Debug/net8.0/ObligatorioBDNR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioBDNR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1253b657776d0597f01082b38d7ff1873d5667f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b61866317c7292bed5b0b2f2d90c865a90b27d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioBDNR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioBDNR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
